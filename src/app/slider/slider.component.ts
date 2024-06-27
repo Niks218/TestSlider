eb8fce4ce343d42bc15232416869ac871f336cdc
Nikita Tsykunov <nikita.tsykunovv@gmail.com>
2024-06-27 22:49:29 +0300

    Add autoscrolling

--- src/app/slider/slider.component.ts
+++ src/app/slider/slider.component.ts
@@ -1,8 +1,8 @@
-import {Component, OnInit} from "@angular/core";
+import {Component, OnDestroy, OnInit, signal, AfterViewInit, ElementRef} from "@angular/core";
 import {SliderFrameComponent} from "./slider-frame/slider-frame.component";
 import {AsyncPipe, NgForOf, NgIf} from "@angular/common";
 import {Slide, SliderService} from "./slider.service";
-import {Observable} from "rxjs";
+import {interval, Observable} from "rxjs";
 
 @Component({
   standalone: true,
@@ -16,15 +16,36 @@ import {Observable} from "rxjs";
   templateUrl: 'slider.component.html',
   styleUrl: './slider.component.css'
 })
-export class SliderComponent implements OnInit {
+export class SliderComponent implements OnInit, OnDestroy, AfterViewInit {
 
   slides$!: Observable<Slide[]>
-  currentSlide = 0
+  readonly currentSlide = signal(0)
+  readonly interval = setInterval(() => this.currentSlide.update(currentValue => currentValue + 1), 10_000)
 
-  constructor(private sliderService: SliderService) {
+  constructor(private sliderService: SliderService, private el: ElementRef<HTMLDivElement>) {
   }
 
   ngOnInit() {
     this.slides$ = this.sliderService.getBanners()
+  }
+
+  ngOnDestroy() {
+    clearInterval(this.interval);
+  }
+
+  ngAfterViewInit() {
+    interval(10_000).subscribe(() => {
+      const slider = this.el.nativeElement.firstElementChild;
+
+      if (!slider) {
+        return
+      }
+
+      if (slider.scrollLeft == slider.scrollWidth - slider.clientWidth) {
+        slider.scrollTo({left: 0, behavior: "smooth"})
+      } else {
+        slider.scrollBy({left: slider.clientWidth, behavior: "smooth"})
+      }
+    })
   }
 }
